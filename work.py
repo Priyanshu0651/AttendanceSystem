e6e5177c3858b98becc7f42a70c1a303e3f4c8d6
Priyanshu0651 <priykr@iitk.ac.in>
2022-05-28 12:52:16 +0530

    chl

--- work.py
+++ work.py
@@ -1,13 +1,1 @@
-import os
-lines = []
-
-with open("requirements.txt","r+") as fp:
-    for l in fp:
-        if("@" not in l):
-            lines.append(l)
-
-os.remove("requirements.txt")
-my_file = open("requirements.txt", "w")
-
-for l in lines:
-    my_file.write(l)
+import pandas as pd
